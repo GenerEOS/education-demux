9d83bdceebfbd035a76894b2076363ba1f5a788e
ignatiusrevi <ignatiusrevi38@gmail.com>
2019-03-20 11:39:21 +1100

    account model removed

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,9 +1,7 @@
 import BlockIndexState from './block-index-state.model'
 import Post from './post.model'
-import Account from './account.model'
 
 export {
   BlockIndexState,
-  Post,
-  Account
+  Post
 }
